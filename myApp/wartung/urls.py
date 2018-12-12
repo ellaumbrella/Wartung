a5b9d42d919280bf05e6394f268f4a1f66f55777
Ella <ella.hiesch@gmail.com>
2018-12-12 15:56:13 +0100

    views.py muss noch was gemacht werden beim defWartungenlist, meine Maus ist in der VM hops gegangen und nichts reagiert mehr ...lol

--- myApp/wartung/urls.py
+++ myApp/wartung/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('wartungen/', views.WartungenListView.as_view(), name='wartungen'),
-
+	path('wartungen/', views.wartungenlist, name='Wartungen'),
+#	path('wartungen/<int:pk>', views.WartungenDetailView.as_view(), name='Wartungen-Details'),
 ]
--- myApp/wartung/views.py
+++ myApp/wartung/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from wartung.models import Wartungen
 from django.shortcuts import render
 from django.views import generic
+from django.template.loader import get_template
+
 
 # Create your views here.
 def index(request): 
@@ -14,5 +16,35 @@ def index(request):
 
 	return render(request, 'index.html', context=context)
 
-class WartungenListView(generic.ListView):
-	model = Wartungen
+
+def wartungenlist(request):
+	#wartung_list = Wartungen.objects.all().count()
+	w = loader.get_template('wartungen/wartungen_list.html')
+
+	return render
+
+# class WartungenListView(generic.ListView):
+# 	model = Wartungen
+# 	context_object_name = 'Wartungsliste'
+# 	template_name = 'wartungen/wartungen_list'
+#
+# 	def get_queryset(self):
+# 		return Wartungen.objects.all()
+#
+#
+# 	#def get_context_data(self, **kwargs):
+# 		# Call the base implementation first to get the context
+# 	#	context = super(WartungenListView, self).get_context_data(**kwargs)
+# 		# Create any data and add it to the context
+# 	#	context['some_data'] = 'This is just some data'
+# 	#	return context
+# class WartungenDetailView(generic.DetailView):
+# 	model = Wartungen
+#
+# 	def wartung_detail_view(request, primary_key):
+# 		try:
+# 			wartungen = Wartungen.objects.get(pk=primary_key)
+# 		except Book.DoesNotExist:
+# 			raise Http404('Es existieren keine Wartungen')
+#
+# 		return render(request, 'wartung/wartungen_detail.html', context={'Wartung': wartungen})
