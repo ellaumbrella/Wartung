082145db1443e1ec8e0c46ae281b376dbc8c1c1a
maxiking203 <Maximilian.Sachmann@gmx.de>
2018-12-18 11:01:49 +0100

    not working create

--- myApp/wartung/models.py
+++ myApp/wartung/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 from django import forms
+from django.urls import reverse
 
 
 # Create your models here.
@@ -11,13 +12,10 @@ class Wartungen(models.Model):
 	wartungen_name = models.CharField(max_length=50, verbose_name='Wartungsname')
 	wartungen_text = models.TextField(max_length=200, verbose_name='Wartungstext')
 	domain = models.CharField(max_length=100, verbose_name='Domain')
-	startzeit = models.DateTimeField('beginnt:')
-	endzeit = models.DateTimeField('endet:')
+	startzeit = models.DateTimeField('beginnt:', null=True)
+	endzeit = models.DateTimeField('endet:', null=True)
 
-
-
-
-	pub_date = models.DateTimeField(editable=False)
+	#pub_date = models.DateTimeField(editable=False)
 
 	def save(self, *args, **kwargs):
 		if not self.id:
@@ -28,6 +26,8 @@ class Wartungen(models.Model):
 	def __str__(self):
 		return self.wartungen_text
 
+	def get_absolute_url(self):
+		return reverse('Wartungen-create', kwargs={'pk': self.pk})
 
 
 
